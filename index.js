e2a6c66a881de7aea067a398eb0c506c8ab022fe
richardchan2 <richard.chan2@ucalgary.ca>
2017-03-10 00:35:52 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -9,10 +9,46 @@ http.listen( port, function () {
 });
 
 app.use(express.static(__dirname + '/public'));
-
+var nicknames = [];
+var chathistory = [];
 // listen to 'chat' messages
 io.on('connection', function(socket){
-    socket.on('chat', function(msg){
-	io.emit('chat', msg);
+    console.log('A user connected');
+	socket.on('new user', function(data){
+		var temp1 = Math.random().toString(36).substr(2, 9);
+		socket.emit('clientConnected',{userInfo: 'You are ' + temp1});
+		socket.emit('loadhistory', chathistory);
+		socket.nickname = temp1;
+		nicknames.push(temp1);
+		io.sockets.emit('usersonline', nicknames);
+	});
+	
+	socket.on('chat', function(msg){
+		var hours = new Date();
+		var mins = new Date();
+		if (msg.startsWith('/nickcolor')) {
+			socket.color = msg.slice(11);
+		}else if (msg.startsWith('/nick')) {
+				if(nicknames.indexOf(msg.slice(6)) !=-1) {
+					socket.emit('userError', {error: 'Username already in use.'});
+				}else{
+					var temp2 = nicknames.indexOf(socket.nickname);
+					socket.nickname = msg.slice(6);
+					nicknames[temp2] = socket.nickname;
+					socket.emit('identityChange', {userInfo: 'You are ' + socket.nickname});
+					io.sockets.emit('usersonline', nicknames);
+				}
+		}else{
+			var chatm = hours.getHours() + ':' +mins.getMinutes() + ' ' + socket.nickname + ': ' + msg;
+			chatm = chatm.toString();
+			chathistory.push(chatm);
+			io.sockets.emit('chat', {color: socket.color, message: msg, nick: socket.nickname, timestamp: hours.getHours() + ':' + mins.getMinutes()});
+		}
+	});	
+	
+    socket.on('disconnect', function (){
+        console.log('A user disconnected');
+		nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+		io.sockets.emit('usersonline', nicknames);
     });
 });
--- public/client.js
+++ public/client.js
@@ -1,12 +1,50 @@
 // shorthand for $(document).ready(...)
 $(function() {
     var socket = io();
-    $('form').submit(function(){
-	socket.emit('chat', $('#m').val());
-	$('#m').val('');
-	return false;
+	var $messages = $('#messages');
+	var $messageBox = $('#m');
+	var $messageForm = $('form');
+	var $users = $('#users');
+	var $changeUserForm = $('#changeUserForm');
+	var $changeUserBox = $('#c');
+	var $identity = $('#identity');
+
+	socket.emit('new user', 'New client has connected');
+	socket.on('clientConnected', function(data) {
+		$identity.html(data.userInfo);
+		$messages.append($('<li>').text(data.userInfo));
+	});
+	
+	socket.on('loadhistory', function(data) {
+		var chathistory = '';
+		for(x = 0; x < data.length; x++) {
+			$messages.append($('<li>').text(data[x]));
+		}
+	});
+	
+	socket.on('usersonline', function(data) {
+		var html ='';
+		for(x = 0; x < data.length; x++) {
+			html = html + data[x] + "<br/>";
+		}
+		$users.html(html);
+	});
+	
+    $messageForm.submit(function(e){
+		e.preventDefault();
+		socket.emit('chat', $messageBox.val());
+		$messageBox.val('');
     });
+	
+	socket.on('userError', function(msg){
+		$messages.append($('<li>').text(msg.error));
+	});
+	
+	socket.on('identityChange', function(data) {
+			$identity.html(data.userInfo);
+	});
+	
     socket.on('chat', function(msg){
-	$('#messages').append($('<li>').text(msg));
-    });
+		$messages.append($('<li>').html(msg.timestamp + ' ' + '<span style="color:' + msg.color + '"' + '>' + msg.nick + '</span>' + ': ' + msg.message));
+	});
 });
